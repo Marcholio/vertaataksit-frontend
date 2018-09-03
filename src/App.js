1b023f29d72cfc48018ee3b0728d404d635e8a91
markus <marcholio09@gmail.com>
2018-09-03 19:25:22 +0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Home from './containers/customer/Home';
+import Admin from './containers/admin/Admin';
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="app">
+          <header className="app-header">
+            <h1 className="app-title">Welcome to React</h1>
+            <div id="main-menu">
+              <Link to="/">
+                <button>home</button>
+              </Link>
+              <Link to="/admin">
+                <button>admin</button>
+              </Link>
+            </div>
+          </header>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/admin" component={Admin} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/containers/admin/Admin.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const admin = () => <div>Admin home</div>;
+
+export default admin;
